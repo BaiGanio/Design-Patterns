bfc74ca4845312e7241e9c7d1cf68ee7fb31d36b
BaiGanio <lubenkikov@gmail.com>
2017-12-26 18:34:55 +0200

    Some refinements on Template method pattern project.

---
+++ DP Workshops/StarbuzzCoffeeBarista/Startup_TemplateMethodPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StarbuzzCoffeeBarista
+{
+    class Startup_TemplateMethodPattern
+    {
+        static void Main(string[] args)
+        {
+            Coffee coffee = new Coffee();
+            coffee.PrepareRecipe();
+            Tea tea = new Tea();
+            tea.PrepareRecipe();
+        }
+    }
+}
