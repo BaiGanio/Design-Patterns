e356e98c69875cdf646fdd53b6279915466393af
BaiGanio <lubenkikov@gmail.com>
2018-01-02 00:41:16 +0200

    Few updates on Singleton project.

--- DP Workshops/ChocolateFactory/LazySingleton.cs
+++ DP Workshops/ChocolateFactory/LazySingleton.cs
@@ -3,22 +3,22 @@
 namespace ChocolateFactory
 {
     /* Sealed restrict the inheritance */
-    public sealed class Singleton
+    public sealed class LazySingleton
     {
         /*Lazy objects are thread safe*/
-        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
-        public static Singleton GetInstance
+        private static readonly Lazy<LazySingleton> instance = new Lazy<LazySingleton>(() => new LazySingleton());
+        public static LazySingleton GetInstance
         {
             get { return instance.Value; }
         }
         /* Private constructors ensures that object is not instantiated other than within the class itself */
-        private Singleton() { }
+        private LazySingleton() { }
 
     }
 }
 
 /*
- * Lazy loading improves the prformance.
+ * Lazy loading improves the performance.
  * Avoid unnecessary load.
  * Reduces memory footprint at startup.
  * Faster application load 
---
+++ DP Workshops/SingletonBasics/Models/Log.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SingletonBasics.Models
+{
+    public sealed class Log : ILog
+    {
+        private static readonly Lazy<Log> instance
+            = new Lazy<Log>(() => new Log());
+
+        private Log()
+        { }
+
+        public static Log GetInstance
+        {
+            get { return instance.Value; }
+        }
+
+        public void LogException(string message)
+        {
+            string fileName = string.Format("Exception ... {0} .log", DateTime.Now.ToFileTime());
+            string logFilePaths = string.Format(@"D:\PROGRAMING\MY PROJECTS\Design Patterns\ExceptionLogging\{0}", fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(new string('*', 20));
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(message);
+            MemoryStream mStrm = new MemoryStream(Encoding.UTF8.GetBytes(logFilePaths));
+            using (StreamWriter writer = new StreamWriter(mStrm))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+
+        public void LogSomething(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
